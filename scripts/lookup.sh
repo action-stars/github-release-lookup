45cc639ceaf03471bb3ab08ba5f922e07e228f76
Steve Hipwell <steve.hipwell@gmail.com>
2025-05-13 10:13:52 +0100

    chore: Add automation (#14)
    Signed-off-by: Steve Hipwell <steve.hipwell@gmail.com>

--- scripts/lookup.sh
+++ scripts/lookup.sh
@@ -1,16 +1,14 @@
 #! /bin/env bash
 set -euo pipefail
 
-if [[ "${VERSION}" == "latest" ]]
-then
+if [[ "${VERSION:-latest}" == "latest" ]]; then
   version="$(gh api "repos/${OWNER}/${REPOSITORY}/releases/latest" -q .tag_name)"
 else
   version="$(gh api "repos/${OWNER}/${REPOSITORY}/releases/tags/${TAG_PREFIX}${VERSION}" -q .tag_name)"
 fi
 
-if [[ -n "${TAG_PREFIX}" ]]
-then
-  version="${version/#$TAG_PREFIX}"
+if [[ -n "${TAG_PREFIX}" ]]; then
+  version="${version/#${TAG_PREFIX}/}"
 fi
 
-echo "version=${version}" >> "${GITHUB_OUTPUT}"
+echo "version=${version}" >>"${GITHUB_OUTPUT}"
